fc4806de3ffcd6b08491ad5fc69b0bb8e156566c
Yadunandana Acharya <51967645+Yadunandanaacharya@users.noreply.github.com>
2020-10-23 22:17:03 +0530

    Solving Fibonacci problem.
    You need to put if consition here, if 0 and 1 then type anything you want else we need to return iterable total count.

---
+++ Day22.js
@@ -0,0 +1,6 @@
+// Started working completely on JS, carrershift towards Web-dev
+function colorInvert(rgb) {
+	objectis = rgb
+	return [255-objectis[0],255-objectis[1],255-objectis[2]]
+	
+}
---
+++ Day23.py
@@ -0,0 +1,40 @@
+# # Reverse a string wthout using any inbuitl function
+# def reversestring(txt):
+#     string = ''
+#     for i in range(len(txt)-1,-1,-1):
+#         string = string + txt[i]
+#     print(string)
+#     reve = ''.join([txt[i] for i in range(len(txt)-1,-1,-1)])
+#     print(reve)
+# print(reversestring('Hai'))
+
+# def is_palindrome(txt):
+# 	return ''.join([txt[i] for i in range(len(txt)-1,-1,-1)]) == txt
+	
+
+def fibonacci(n):
+    if n==0 or n==1:
+        output = 1
+        # print('1',output)
+        return output
+    else:
+        output = n + fibonacci(n-1)
+        # print('2',output)
+        return (output)
+
+# print(fibonacci(10))
+
+
+def fibis(n):
+    start = 1
+    end = 2
+    for i in range(0,n):
+        if i<=2:
+            # Write whatever here
+            i='hai'
+        else:
+            next = start + end
+            start = end
+            end = next
+    print(str(next))
+print(fibis(10))
